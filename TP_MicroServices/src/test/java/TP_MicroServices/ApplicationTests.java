4f61742a68ac01e77e0f38f18e45b3a5322506e3
GaetanHardy <hardygaetan@outlook.fr>
2022-01-21 15:58:25 +0100

    Ajouts dossiers TP

---
+++ TP_MicroServices/src/test/java/TP_MicroServices/ApplicationTests.java
@@ -0,0 +1,13 @@
+package TP_MicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
