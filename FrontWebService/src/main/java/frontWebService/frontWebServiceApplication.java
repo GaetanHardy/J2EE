1b888a9860c726386aa5f9366e16ad85b00ea202
GaetanHardy <hardygaetan@outlook.fr>
2022-01-21 16:58:26 +0100

    Ajout dossiers TP micro-service

---
+++ FrontWebService/src/main/java/frontWebService/frontWebServiceApplication.java
@@ -0,0 +1,31 @@
+package frontWebService;
+
+import freemarker.core.ReturnInstruction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class frontWebServiceApplication {
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @GetMapping("/")
+    public String hello() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("microservice1");
+        ServiceInstance test = instances.get(0);
+        String hostname = test.getHost();
+        int port = test.getPort();
+        RestTemplate restTemplate = new RestTemplate();
+        String microservice1Address = "http://" + hostname + ":" + port;
+        ResponseEntity<String> response =
+                restTemplate.getForEntity(microservice1Address, String.class);
+        String s = response.getBody();
+        return s;
+    }
+}
